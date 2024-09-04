c30aabeceb0b951bd5e788e8868996fc115a0be9
Brandon Hamilton <bham2015@gmail.com>
2024-09-04 18:44:31 -0500

    The changes in this commit will add the three variables needed for the code.

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+Display a string for the user.
+Set three variables to get the numbers for the combination.
+Create a dialog box with the string and code
+    - Pull the `<p>` tag from the HTML file and display it in the dialog box.
+    - Look up how to pull the tag and create the dialog box
+*/
+
+const a = 5 * 2;
+const b = a * 4;
+const c = b - 1;
+
