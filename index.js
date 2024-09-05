86b1facb8df2faff6e242350bc6e33aa043dc577
Brandon Hamilton <bham2015@gmail.com>
2024-09-04 20:16:02 -0500

    The changes in this commit will add the confirm box to show the user the message with the code.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Display a string for the user.
+Display a string for the user. This is in the HTML file.
 Set three variables to get the numbers for the combination.
 Create a dialog box with the string and code
     - Pull the `<p>` tag from the HTML file and display it in the dialog box.
@@ -11,4 +11,7 @@ const b = a * 4;
 const c = b - 1;
 
 const vault = document.getElementsByTagName("p")[0].innerHTML
+const vault2 = document.getElementsByTagName("p")[1].innerHTML
+
+window.confirm(vault + "\n" + "\n" + vault2 + " " + a + "-" + b + "-" + c + ".")
 
